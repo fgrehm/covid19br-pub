da5fba65c192163bb5d54f00434e2cc5b09051be
Fabio Rehm <fgrehm@gmail.com>
2020-04-30 00:20:18 -0300

    Fix ref to rails config

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,9 +1,9 @@
 class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  if Rails.configuration.webhook_user.present?
-    http_basic_authenticate_with name: Rails.configuration.webhook_user,
-                                 password: Rails.configuration.webhook_password
+  if Rails.configuration.x.webhook_user.present?
+    http_basic_authenticate_with name: Rails.configuration.x.webhook_user,
+                                 password: Rails.configuration.x.webhook_password
   end
 
   def create
