8357f8c430af41b89f967cce614e2bbca774b8d5
Fabio Rehm <fgrehm@gmail.com>
2020-05-05 20:31:31 -0300

    Fix ordering when a filter by state is specified

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,7 +1,7 @@
 class ContentsController < ApplicationController
   def index
     @contents = Content.order(published_at: :desc).includes(:content_source)
-    @contents = Content.by_state(params[:state_slug]) if params[:state_slug].present?
+    @contents = @contents.by_state(params[:state_slug]) if params[:state_slug].present?
     @contents = @contents.page(params[:page])
   end
 end
