ddf54f9c98fe8a7609f654f8126f108c3ac08e58
Fabio Rehm <fgrehm@gmail.com>
2020-05-09 23:40:17 -0300

    "Infinite scroll" and twitter widgets
    This concludes the MVP for spreading the word about the project, yes
    there's a lot of ugly code but things work.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,5 +3,7 @@ class ContentsController < ApplicationController
     @contents = Content.order(published_at: :desc).includes(:content_source)
     @contents = @contents.by_state(params[:state_slug]) if params[:state_slug].present?
     @contents = @contents.page(params[:page])
+
+    render :index, layout: false if request.xhr?
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,65 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("jquery");
 
-import "../styles/application"
+import "../styles/application";
+
+function initTwitter() {
+  return window.twttr = (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    t._e = [];
+    t.ready = function(f) {
+      t._e.push(f);
+    };
+
+    return t;
+  }(document, "script", "twitter-wjs"));
+}
+
+document.addEventListener("turbolinks:load", function() {
+  initTwitter().ready(renderTweets);
+
+  $(document).on("click", "#see-more-contents-btn", function(e) {
+    e.preventDefault();
+    const $button = $(e.currentTarget);
+    $button.addClass("is-secondary").removeClass("is-primary");
+    $.get($button.data("url"), function(resp) {
+      const $page = $(resp)
+      $(".contents").append($page.find(".contents").children());
+      $(".see-more").replaceWith($page.find(".see-more"));
+      renderTweets();
+    });
+  });
+});
+
+function renderTweets() {
+  $("[data-tweet-id]").each(function() {
+    const $widget = $(this).find("twitter-widget");
+    if ($widget.length > 0) return;
+
+    twttr.widgets.createTweet(
+      this.dataset.tweetId,
+      this
+    ).then((e) => {
+      if (e) {
+        $(e).siblings(".tweet-wrapper").remove();
+      } else {
+        // alert(`Esbarrou num tweet removido!`);
+      }
+    });
+  });
+}
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
