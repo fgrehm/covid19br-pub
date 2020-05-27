7e89d1917998cbc26092a265c2f806453d266f3f
Fabio Rehm <fgrehm@gmail.com>
2020-05-27 11:04:24 -0300

    OG meta tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -128,9 +128,9 @@ window.initStatsChart = (el) => {
   var ctx = el.getContext('2d');
   new Chart(ctx, config);
 }
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+// Copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag
+// 'rails.png' %>) or the `imagePath` JavaScript helper below.
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
